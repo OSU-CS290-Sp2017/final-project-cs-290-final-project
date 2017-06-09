2c2754e71c8a45a7ed4438bd93c1e4c6de19731f
Cole Jones <jonecole@flip3.engr.oregonstate.edu>
2017-06-08 21:35:09 -0700

    Last update for tonight. No CSS, but the server serves correctly

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
 
-var postData = require('./postData');
+var postsData = require('./postData');
 var app = express();
 var port = process.env.PORT || 3000;
 
@@ -11,10 +11,29 @@ app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res, next) {
 	console.log("== URL params for request:", req.params);
-	
+	console.log("== postData:", postsData);
 	var templateArgs = {
-		posts: postData
+		posts: postsData
 	};
+	
+	res.render('postPage', templateArgs);
+});
+
+app.get('/:post', function (req, res, next) {
+	console.log("== URL params for request:", req.params);
+	console.log("== req.params.post:", req.params.post);
+	
+	var post = req.params.post;
+	var postData = [postsData[post]];
+	
+	if(postData) {
+		var templateArgs = {
+			posts: postData,
+		};
+	}
+	else {
+		next();
+	}
 	
 	res.render('postPage', templateArgs);
 });
