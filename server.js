4e5d2901fb98ce8209d37fe117bfa7e90dc35596
Cole Jones <jonecole@flip3.engr.oregonstate.edu>
2017-06-08 19:46:39 -0700

    Adding all files so server will work.

---
+++ server.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var postData = require('./postData');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res, next) {
+	console.log("== URL params for request:", req.params);
+	
+	var templateArgs = {
+		posts: postData
+	};
+	
+	res.render('postPage', templateArgs);
+}
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', function (req, res) {
+	res.status(404).render('404Page');
+});
+
+app.listen(port, function () {
+	console.log("== Server listening on port: ", port);
+});
